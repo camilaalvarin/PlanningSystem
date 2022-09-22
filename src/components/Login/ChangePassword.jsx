992b3282b54d6853a6f290051f1c65196edb44b5
Flor Colaiacovo <florcolaiacovo@gmail.com>
2022-09-22 09:50:14 -0300

    actualizacion

--- src/components/Login/ChangePassword.jsx
+++ src/components/Login/ChangePassword.jsx
@@ -43,7 +43,8 @@ const ChangePassword = () => {
                             Change
                         </Button>
                         </NavLink>
-                        
+                    
+                    
                     </Form>
 
                 </div>
